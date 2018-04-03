630239397747418d8ce7f61b73a4f59011765d95
D7703_28 <d7703_28@d7703-28ui-imac.local>
2018-04-03 15:30:56 +0900

    이미지 바꾸기

--- Alien View/ViewController.swift
+++ Alien View/ViewController.swift
@@ -10,16 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var Counter = 1
+    var direction = true;
+    
+    @IBOutlet weak var Alienimage: UIImageView!
+    @IBOutlet weak var Index: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // 1st 이미지가 출력
+        Alienimage.image = UIImage(named: "frame1.png")
+        Index.text = "1"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func Updateimage(_ sender: Any) {
+        if Counter == 5 {
+            direction = false
+        } else if Counter == 1{
+            direction = true
+        }
+        if direction == true {
+            Counter += 1;
+        } else if direction == false {
+            Counter = Counter - 1
+        }
+        Alienimage.image = UIImage(named: "frame\(Counter).png")
+        Index.text = String(Counter)
+        }
     }
-
-
-}
-
